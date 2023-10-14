0d2784fb190be43a1fb336c272c48e2a080bf350
nyddanao11 <112321001+nyddanao11@users.noreply.github.com>
2023-10-14 12:15:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -377,7 +377,7 @@ app.get('/api/user', async (req, res) => {
 
 
 
-const PORT = process.env.SERVER_PORT||3000;
+const PORT = process.env.SERVER_PORT||3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
