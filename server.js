bb85679308465f5c4bb9cc07e0eefd2efc5e9a9c
nyddanao11 <112321001+nyddanao11@users.noreply.github.com>
2024-01-07 18:47:08 +0000

    jan72024

--- server.js
+++ server.js
@@ -32,20 +32,20 @@ require('dotenv').config({ path: 's3.env' });
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'viaduct.proxy.rlwy.net',
-    port: 23175,
-    user: 'postgres',
-    password:'2BE2-1Be243GFFbda*DBaaFFbgDg25b6',
-    database: 'railway',
+    host: process.env.RAILWAY_TCP_PROXY_DOMAIN,
+    port: process.env.RAILWAY_TCP_PROXY_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
   },
 });
 
 const pool = new Pool({
-   host: 'viaduct.proxy.rlwy.net',
-    port: 23175,
-    user: 'postgres',
-    password:'2BE2-1Be243GFFbda*DBaaFFbgDg25b6',
-    database: 'railway',
+   host: process.env.RAILWAY_TCP_PROXY_DOMAIN,
+    port: process.env.RAILWAY_TCP_PROXY_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
   
 });
 
