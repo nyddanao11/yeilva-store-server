212d19e06f9118df699da6905a2c8830f439f313
Fernando Arong <112321001+nyddanao11@users.noreply.github.com>
2025-06-08 20:03:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -775,6 +775,27 @@ app.get('/api/productsearch', async (req, res) => {
   }
 });
 
+app.get('/api/alldealsproduct', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM products WHERE discount > 0 ');
+  // Format data
+    const formattedData = result.rows.map((product) => ({
+      ...product,
+
+      thumbnails: product.thumbnails?.[0]?.split(/\s+/).map((thumbnail) =>
+        thumbnail.replace(/"/g,'')
+      ) || [],
+    }));
+
+    console.log('Formatted Products Data:', formattedData);
+    res.status(200).json(formattedData); // Send formattedData
+    
+  } catch (error) {
+    console.error('Error fetching featured products:', error);
+    res.status(500).json({ error: 'Failed to fetch featured products' });
+  }
+});
+
 app.get('/api/featuredproducts', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM products WHERE featured = TRUE');
